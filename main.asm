d4f4850b6d5355f33a6d46c0aef081f0bd7117b5
Manoel Vieira <vieiranetoc@gmail.com>
2018-09-21 00:17:09 -0300

    jumping back to main

--- main.asm
+++ main.asm
@@ -1,6 +1,13 @@
 .text
 main: 
-	j openFile
+	jal openFile
+	li $v0, 4
+	la $a0, backtomain
+	syscall
+	li $v0, 10
+	syscall
+	
+	
 	
 
 
@@ -15,18 +22,23 @@ openFile:
 	li $a2, 0 # mode 0
 	syscall
 	move $s0, $v0
+	addi $sp, $sp, -4
+	# needs to queue value from $ra to go back to main
+	sw $ra, 0($sp)
 	jal openFileError
-	#needs to queue value from $ra to go back to main
-	j exit
+	# dequeue to jump back to main
+	lw $t1, 0($sp)
+	addi $sp, $sp, 4
+	jr $t1
 
 openFileError:
 	# Print out if syscall for opening file was unsuccessful
 	move $t0, $v0
 	slt $t1, $t0, $zero
-	bne $t1, $zero, exit
+	bne $t1, $zero, exitError
 	jr $ra
 
-exit:
+exitError:
 	# exit lable is called everytime something goes wrong
 	# syscall 10 exits
 	la $a0, exitMessage
@@ -39,3 +51,4 @@ exit:
 .data
 	inFilename:	.asciiz "img.bmp" #defines filename for opening
 	exitMessage:	.asciiz "Something went wrong"
+	backtomain:	.asciiz "back to main"
