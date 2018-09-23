fe3ec593f2813f51a6ac0c819a92b38b33c8a947
Manoel Vieira <vieiranetoc@gmail.com>
2018-09-23 16:58:22 -0300

    error on last lines

--- macros.asm
+++ macros.asm
@@ -6,13 +6,14 @@
 
 
 .macro showImage (%image_pointer, %size)
-	li $t1, 0
-	move $t3, %size
+	move $t0, %image_pointer
+	sll $t3, %size, 2
+	add %image_pointer, %image_pointer, $t3
 loop:
   	lw $t4, (%image_pointer) # move from space to register
 	sw $t4, ($gp)
 	addi $gp, $gp, 4
-	addi $t1, $t1, 1
-	addi %image_pointer, %image_pointer, 4
-	bne $t1, $t3, loop 
+	addi %image_pointer, %image_pointer, -4
+	sub $t1, %image_pointer, $t0
+	bnez $t1, loop
 .end_macro
