6147e601562202270f844b7668a5e9b9db702000
David Potolski Lafetá <32275325+Potolski@users.noreply.github.com>
2018-09-28 10:10:51 -0300

    Adicionará comentários na showImage
    Esse commit adiciona comentários na ShowImage para facilitar o entendimento da função

--- macros.asm
+++ macros.asm
@@ -12,8 +12,7 @@
 	add $t3,%image_pointer,$t3 #fim
 	move $t2,%image_pointer
 
-	
-loop:	
+loop: #vira de cabeça pra cima
 	lw $t6,0($t1)
 	lw $t7,0($t3)
 	add $t4,$t6,$zero
@@ -26,11 +25,12 @@ loop:
 	bne $t3,$t1,loop			
 	
 	move $t1,%image_pointer
-	addi $t8,$zero,512
-loopespelho:
+	addi $t8,$zero,512 #número de linhas que o loop deverá passar 
+	
+loopespelho:#loop das linas
 	add $t3,$t1,2048
-	addi $t9,$zero,256
-loopinterno:
+	addi $t9,$zero,256 #número de repetições da troca de colunas 
+loopinterno:#loop das colunas
 	lw $t6,0($t1)
 	lw $t7,0($t3)
 	add $t4,$t6,$zero
@@ -47,11 +47,10 @@ loopinterno:
 	addi $t8,$t8,-1
 	bne $t8,0,loopespelho
 
-
 	move $t3,%size
 	sll $t3,$t3,2
 	add $t3,%image_pointer,$t3 
-loop2:
+loop2:#loop de impressão
   	lw $t4, ($t2) # move from space to register
 	sw $t4, ($gp)
 	addi $gp, $gp, 4
@@ -61,7 +60,7 @@ loop2:
 
 .end_macro
 
-.macro showBlackWhite (%image_pointer, %size)
+.macro showBlackWhite (%image_pointer, %size) 
 	move $t3, %size
 	sll $t3,$t3,2
 	move $t1, %image_pointer #começo
@@ -80,6 +79,7 @@ loop:
 	addi $t3,$t3,-4
 	bne $t3,$t1,loop			
 	
+	
 	move $t1,%image_pointer
 	addi $t8,$zero,512
 loopespelho:
@@ -102,18 +102,14 @@ loopinterno:
 	addi $t8,$t8,-1
 	bne $t8,0,loopespelho
 
-
-
-
-
 	move $t3,%size
 	sll $t3,$t3,2
 	add $t3,%image_pointer,$t3 
 	addi $t7,$zero,0x007f7f7f #Filtro de cor
 loop2:
   	lw $t4, ($t2) # move from space to register
-  	slt $t5,$t4,$t7
-  	beq $t5,1,preto
+  	slt $t5,$t4,$t7 #compara a cor de t4 com o tom médio de cinza 
+  	beq $t5,1,preto 
  
  branco: addi $t4,$zero,0xffffffff	
  	 j continua
