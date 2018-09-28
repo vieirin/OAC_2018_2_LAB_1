dfe01780fffbc66ea0a86c9247521015371e8de5
David Potolski Lafetá <32275325+Potolski@users.noreply.github.com>
2018-09-28 09:56:53 -0300

    Espelhará a imagem
    Esse commit irá deixar a imagem igual à original, com a possibilidade de torná-la preto e branco(binarização de cores)

--- macros.asm
+++ macros.asm
@@ -11,6 +11,7 @@
 	move $t1, %image_pointer #começo
 	add $t3,%image_pointer,$t3 #fim
 	move $t2,%image_pointer
+
 	
 loop:	
 	lw $t6,0($t1)
@@ -23,6 +24,29 @@ loop:
 	addi $t1, $t1,4
 	addi $t3,$t3,-4
 	bne $t3,$t1,loop			
+	
+	move $t1,%image_pointer
+	addi $t8,$zero,512
+loopespelho:
+	add $t3,$t1,2048
+	addi $t9,$zero,256
+loopinterno:
+	lw $t6,0($t1)
+	lw $t7,0($t3)
+	add $t4,$t6,$zero
+	add $t6,$t7,$zero
+	add $t7,$t4,$zero
+	sw $t6, 0($t1)
+	sw $t7 ,0($t3)
+	addi $t1, $t1,4
+	addi $t3,$t3,-4
+	addi $t9,$t9,-1
+	bne $t9,0,loopinterno
+	
+	add $t1,$t1,1024
+	addi $t8,$t8,-1
+	bne $t8,0,loopespelho
+
 
 	move $t3,%size
 	sll $t3,$t3,2
@@ -55,6 +79,32 @@ loop:
 	addi $t1, $t1,4
 	addi $t3,$t3,-4
 	bne $t3,$t1,loop			
+	
+	move $t1,%image_pointer
+	addi $t8,$zero,512
+loopespelho:
+	add $t3,$t1,2048
+	addi $t9,$zero,256
+loopinterno:
+	lw $t6,0($t1)
+	lw $t7,0($t3)
+	add $t4,$t6,$zero
+	add $t6,$t7,$zero
+	add $t7,$t4,$zero
+	sw $t6, 0($t1)
+	sw $t7 ,0($t3)
+	addi $t1, $t1,4
+	addi $t3,$t3,-4
+	addi $t9,$t9,-1
+	bne $t9,0,loopinterno
+	
+	add $t1,$t1,1024
+	addi $t8,$t8,-1
+	bne $t8,0,loopespelho
+
+
+
+
 
 	move $t3,%size
 	sll $t3,$t3,2
